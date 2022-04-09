5a4c7b43f9895daaa8a5a3e2fc91aef78ff345ee
Germán A. Gómez <germang08@hotmail.com>
2022-04-09 20:17:44 -0300

    fixes

--- api/routes/api/lista.php
+++ api/routes/api/lista.php
@@ -7,10 +7,4 @@ use Illuminate\Support\Facades\Route;
  * -------------------------------------- **/
 Route::group(['prefix' => 'lista'], function () {
     Route::get('', 'Lista\ListaController@all');
-
-    // Route::group(['middleware' => ['auth:api','auth.isAdmin']], function () {
-    //     Route::post('', 'Users\UserController@insert')->middleware('user.data', 'user.password', 'user.email.repeat');
-    //     Route::put('', 'Users\UserController@update')->middleware('user.data');
-    //     Route::delete('/{email}', 'Users\UserController@delete')->middleware('user.email');
-    // });
 });
